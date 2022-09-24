da2ed313c14518e2e55063905f840d54c3cefc09
borodov.m <borodov.m@gmail.com>
2022-09-24 17:43:01 +0300

    added Examples 17 and 18

---
+++ Example_017/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Console.WriteLine("Введите координаты точки:");
+// Console.Write("x = ");
+// double x = Convert.ToDouble(Console.ReadLine());
+// Console.Write("y = ");
+// double y = Convert.ToDouble(Console.ReadLine());
+
+Random random = new Random();
+int x = random.Next(-10,11);
+int y = random.Next(-10,11);
+//Console.WriteLine($"A({x}, {y})");
+
+void Check(int x, int y)
+{
+    if ((x>0)&&(y>0))
+    {
+        Console.WriteLine($"Точка A({x},{y}) лежит в первом квадранте");
+    }
+    else if ((x>0)&&(y<0))
+    {
+        Console.WriteLine($"Точка A({x},{y}) лежит в четвертом квадранте");
+    }
+    else if ((x<0)&&(y>0))
+    {
+        Console.WriteLine($"Точка A({x},{y}) лежит в втором квадранте");
+    }
+    else if ((x<0)&&(y<0))
+    {
+        Console.WriteLine($"Точка A({x},{y}) лежит в третьем квадранте");
+    }
+    else
+    {
+        Console.WriteLine($"Точка A({x},{y}) лежит в на оси");
+    }
+}
+
+Check(x,y);
+
+// int result;
+
+// if (x>0)
+// {
+//     if (y>0)
+//     {
+//         result = 1;
+//     }
+//     else
+//     {
+//         result = 4;
+//     }
+// }
+// else
+// {
+//     if (y>0)
+//     {
+//         result = 2;
+//     }
+//     else
+//     {
+//         result = 3;
+//     }
+// }
+//Console.WriteLine($"Точка A({x},{y}) находится в квадранте {result}");
