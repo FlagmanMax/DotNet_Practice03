684aa54f32eda86802dd7f35518110700ef5fb03
borodov.m <borodov.m@gmail.com>
2022-09-24 18:14:57 +0300

    Added example 22

---
+++ Example_021/Program.cs
@@ -0,0 +1,12 @@
+﻿Random random = new Random();
+
+int x1 = random.Next(-10,11);
+int y1 = random.Next(-10,11);
+Console.WriteLine($"Координаты точки А({x1},{y1}):");
+
+int x2 = random.Next(-10,11);
+int y2 = random.Next(-10,11);
+Console.WriteLine($"Координаты точки B({x2},{y2}):");
+
+double result = Math.Sqrt((x2-x1)*(x2-x1) +(y2-y1)*(y2-y1));
+Console.WriteLine("Расстояние между точками A и B равно "+ Math.Round(result,3));
---
+++ Example_022/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x<1)
+{
+   Console.WriteLine("Ошибка ввода"); 
+}
+else
+{
+    int count = 1;
+    Console.Write("Число\tКвадрат\r\n");
+    while(count <= x)
+    {
+        Console.Write(count + "\t" + Math.Pow(count,2) + "\r\n");
+        count++;
+    }
+    Console.WriteLine();
+}
+
